95fd016af100fa7549cd427f5234a74576ead0e7
guitolentino <guitolentino@gmail.com>
2024-04-09 17:04:50 -0300

    New features added

--- app.py
+++ app.py
@@ -10,13 +10,25 @@ car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Criar histograma')
 
 if hist_button:
-    st.write('Criando um histograma para o conjunto de dados de anúncios de veículos')
+    st.write('Histograma para o conjunto de dados de anúncios de veículos')
     fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
 
 build_histogram = st.button('Criar gráfico de dispersão')
 if build_histogram:
-    st.write('Criando um histograma para a coluna odometer')
+    st.write('Gráfico de dispersão:')
     fig = px.scatter(car_data, x="odometer", y="price") 
     st.plotly_chart(fig, use_container_width=True)
     
+
+build_checkbox = st.checkbox('Criando um histograma:')
+if build_checkbox:
+    st.write('Histograma:')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+build_checkbox = st.checkbox('Criando um gráfico de dispersão:')
+if build_checkbox:
+    st.write('Gráfico de dispersão:')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
